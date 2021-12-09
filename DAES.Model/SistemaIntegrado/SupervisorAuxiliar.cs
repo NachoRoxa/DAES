8856ede53adf0318d1c9ed877ee9069b902c114b
Ignacio Alfredo Rocha Pavez <irocha@economia.cl>
2021-12-09 12:01:25 -0300

    Creating the controller, class and view

---
+++ DAES.Model/SistemaIntegrado/SupervisorAuxiliar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAES.Model.SistemaIntegrado
+{
+    [Table("SupervisorAuxiliar")]
+    class SupervisorAuxiliar
+    {
+        public SupervisorAuxiliar()
+        {
+
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name="Id")]
+        public int SupervisorAuxiliarId { get; set; }
+
+        public string Rut { get; set; }
+        public string DomicilioLegal { get; set; }
+        public string Telefono { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        /*Lista Representante Legal*/
+
+        /*Lista escritura constitucion y modificacion*/
+
+        /*Lista Modificaciones*/
+
+        /*Lista extracto*/
+
+        /*Lista personas facultadas de supervision*/
+    }
+}
---
+++ DAES.Web.FrontOffice/Controllers/SupervisorAuxiliarController.cs
@@ -0,0 +1,27 @@
+﻿using DAES.Web.FrontOffice.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DAES.Web.FrontOffice.Controllers
+{
+    [Audit]
+    public class SupervisorAuxiliarController : Controller
+    {
+        public class Search
+        {
+            public Search()
+            {
+                Supervisores = new List<SupervisorAuxiliar>();
+            }
+        }
+
+        // GET: SupervisoresAuxiliares
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
